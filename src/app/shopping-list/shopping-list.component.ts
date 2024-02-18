44d01a66f3b263e17a2e67d6c48a864b8f80ee49
apilaka <apilaka@yahoo.ca>
2024-02-18 17:56:24 -0500

    section 4

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-//import { Ingredient } from '../shared/ingredient.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  // ingredients: Ingredient[] = [
-  //new Ingredient('Apples', 5),
-  //  new Ingredient('Tomatoes', 10),
-  // ];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 
   constructor() {}
 
